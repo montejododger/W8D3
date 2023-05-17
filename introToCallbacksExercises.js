4ce2d43b761fe185e872648d09f4818bb5206544
a-wong-8 <awong88@berkeley.edu>
2023-05-17 12:20:58 -0700

    .

--- introToCallbacksExercises.js
+++ introToCallbacksExercises.js
@@ -17,18 +17,20 @@ class Clock {
     // 3. Call printTime.
         this.printTime();
     // 4. Schedule the tick at 1 second intervals.
-        setInterval(this.tick.bind(this), 1000)
+        setInterval(this._tick.bind(this), 1000)
 
   }
 
   printTime() {
     // Format the time in HH:MM:SS
     // Use console.log to print it.
+    console.log(`${this.hours}:${this.minutes}:${this.seconds}`);
   }
 
   _tick() {
     // 1. Increment the time by one second.
     // 2. Call printTime.
+    if (this.seconds === 60)
   }
 }
 
